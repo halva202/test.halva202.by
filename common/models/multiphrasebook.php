023495b7556f55aade897a6588c6696f51ecea6e
halva202 <halva202@gmail.com>
2016-08-31 00:12:18 +0300

    small translation

--- common/models/multiphrasebook.php
+++ common/models/multiphrasebook.php
@@ -20,8 +20,21 @@ class multiphrasebook
 			// /menu
 			'hi'=>'Hi',
 			'introduction'=>'This is mini version of site about me. I do experiments with code here and then upload code to main site. Code of this site is available ',
+			'here'=>'here',
+			'mainSite'=>'Main site',
+			'introduction2'=>"I try to make the site multilingual, in Russian and English (at the bottom of the page there is the possibility of language selection). I create a page at once in Russian and then translate into English. So the",
+			'introduction3'=>'Russian version of the website is richer by information',
 			
 			'web-developer'=>'Web-developer',
+			'web-developer2'=>'I can create sites of any complexity.',
+			
+			'tutor'=>'Tutor',
+			'tutor2'=>'I can explain chemistry, physic, mathematic, web-programming.',
+			'tutor3'=>'Resume (in russian)',
+			
+			'lifestyle'=>'Lifestyle',
+			'lifestyle2'=>'I prefer active lifestyle.',
+			'lifestyle3'=>'My page on facebook',
 		];
 		return $phrasebook;
 	}
@@ -34,9 +47,22 @@ class multiphrasebook
 			'contact'=>'Обратная связь',
 			// /menu
 			'hi'=>'Здравствуй',
-			'introduction'=>'Это миниверсия сайта обо мне. Сейчас, прежде чем что-то выложить на основной сайт, я тестирую тут. Код сайта в открытом доступе ',
+			'introduction'=>'Это миниверсия сайта обо мне. Сейчас, прежде чем что-то выложить на основной сайт, я тестирую тут. Код данного сайта в открытом доступе ',
+			'here'=>'тут',
+			'mainSite'=>'Главный сайт',
+			'introduction2'=>'Сайт пытаюсь делать мультиязычным, на русском и английском (внизу страницы есть возможность выбора языка). Сразу страницу делаю на русском и только потом перевожу на английский. Так что',
+			'introduction3'=>'русская версия сайта более богата информацией',
 			
 			'web-developer'=>'Веб-разработчик',
+			'web-developer2'=>'Могу создавать сайты практически любой сложности.',
+			
+			'tutor'=>'Репетитор',
+			'tutor2'=>'Обучаю физике, химии, математике, веб-программированию.',
+			'tutor3'=>'Резюме',
+			
+			'lifestyle'=>'Образ жизни',
+			'lifestyle2'=>'Предпочитаю активный образ жизни.',
+			'lifestyle3'=>'Моя страница на фэйсбуке',
 		];
 		return $phrasebook;
 	}
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,34 +9,37 @@ $this->title = 'Halva202';
     <div class="jumbotron">
         <h1><?= $phrasebook['hi'] ?>!</h1>
 
-        <p class="lead"><?= $phrasebook['introduction'] ?> <a href="https://github.com/halva202/test.halva202.by" target="_blank">here</a>.&nbsp;</p>
+        <p class="lead"><?= $phrasebook['introduction'] ?> <a href="https://github.com/halva202/test.halva202.by" target="_blank"><?= $phrasebook['here'] ?></a>.&nbsp;</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.halva202.by">halva202.by</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.halva202.by"><?= $phrasebook['mainSite'] ?></a></p>
     </div>
 
     <div class="body-content">
+	
+		<p><span style="color:#8B4513;"><em><?= $phrasebook['introduction2'] ?> </em></span><span style="color:#FF0000;"><em><?= $phrasebook['introduction3'] ?></em></span><span style="color:#8B4513;"><em>.</em></span>
+		</p>
 
         <div class="row">
             <div class="col-lg-4">
                 <h2><a href="/site/webdeveloper"><?= $phrasebook['web-developer'] ?></a></h2>
 
-                <p>I can create sites.</p>
+                <p><?= $phrasebook['web-developer2'] ?></p>
 
-                <p><a class="btn btn-default" href="http://www.upwork.com/o/profiles/users/_~0106dc02a6699ca661/">Resume on upwork &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.upwork.com/o/profiles/users/_~0106dc02a6699ca661/">Resume on upwork.com &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Tutor</h2>
+                <h2><?= $phrasebook['tutor'] ?></h2>
 
-                <p>I can explain chemistry, physic, mathematic, web-programming.</p>
+                <p><?= $phrasebook['tutor2'] ?></p>
 
-                <p><a class="btn btn-default" href="http://uchim.biz/board/0-0-11664-0-17">Resume (in russian) &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://uchim.biz/board/0-0-11664-0-17"><?= $phrasebook['tutor3'] ?> &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Lifestyle</h2>
+                <h2><h2><?= $phrasebook['lifestyle'] ?></h2></h2>
 
-                <p>I prefer active lifestyle.</p>
+                <p><?= $phrasebook['lifestyle2'] ?></p>
 
-                <p><a class="btn btn-default" href="https://www.facebook.com/halva202">My page on facebook &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://www.facebook.com/halva202"><?= $phrasebook['lifestyle3'] ?> &raquo;</a></p>
             </div>
         </div>
 
